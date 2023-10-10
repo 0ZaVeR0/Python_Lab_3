0a7ba4223a838f653469cf1f1c56646c84f6f065
0ZaVeR0 <69377579+0ZaVeR0@users.noreply.github.com>
2023-10-10 14:05:59 +0300

    Update lab_3_2_2.py

--- lab_3_2_2.py
+++ lab_3_2_2.py
@@ -1,14 +1,12 @@
 import math
 
-def koef(n):
+def koef(n: int) -> int:
     c = []
     for i in range (n + 1):
         c.append(int(math.factorial(n) / (math.factorial(n - i) * math.factorial(i))))
-    print(c)
     return (c)
 
-
-def binom(power):
+def binom(power: int) -> str:
     negative = False
     if power == 0:
         return 1
@@ -20,17 +18,20 @@ def binom(power):
     a = str()
     b = str()
     for i in range(power + 1):
-
         a = "a" if power - i == 1 else "a^" + str(power - i) if power - i != 0 else str()
         b = "b" if i == 1 else "b^" + str(i) if i != 0 else str()
 
         ans.append((str(c[i]) if c[i] != 1 else "") + a + b)
-    
+
     ans2  = ans[0]
     for i in range(len(ans)-1):
         ans2 += " + " + ans[i+1] 
     ans2  = ("1/(" + ans2 + ")" if negative else ans2)
     return ans2
 
-n = int(input())
-print(binom(n))
+def main():
+    n = int(input())
+    print(binom(n))
+
+if __name__ == "__main__":
+    main()
