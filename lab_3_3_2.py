124f1f4c75a8ebf9c19c78ffaecff0d4e54f46a9
0ZaVeR0 <69377579+0ZaVeR0@users.noreply.github.com>
2023-10-10 14:06:29 +0300

    Update lab_3_3_2.py

--- lab_3_3_2.py
+++ lab_3_3_2.py
@@ -1,14 +1,19 @@
-def hofstadter_q(x):
-    ans = (rec(n) for n in range (1,x + 1))
-    return ans
+def hofstadter_q(x: int) -> int:
 
-def rec(n):
-    if n == 1 or n == 2:
-        Q = 1
-    if n > 2:
-        Q =  rec(n - rec(n - 1)) + rec(n - rec(n - 2))
-    return Q
+    def rec(n: int) -> int:
+        if n == 1 or n == 2:
+            Q = 1
+        if n > 2:
+            Q =  rec(n - rec(n - 1)) + rec(n - rec(n - 2))
+        return Q
+    
+    for n in range(1,x + 1):
+        yield rec(n)
 
-n = int(input())
-for x in hofstadter_q(n):
-    print (x)
+def main():
+    n = int(input())
+    for x in hofstadter_q(n):
+        print (x)
+
+if __name__ == "__main__":
+    main()
