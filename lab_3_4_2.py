275610212b7702c147f65ffbb01d833819f4c446
0ZaVeR0 <69377579+0ZaVeR0@users.noreply.github.com>
2023-10-10 14:07:01 +0300

    Update lab_3_4_2.py

--- lab_3_4_2.py
+++ lab_3_4_2.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-def nearest_time(times):
+def nearest_time(times: str) -> str:
     time_diff = 9999999999
     for x in times:
         if min(abs(time_convert(x) - c_time()), abs(time_convert(x) + time_convert('24:00:00') - c_time())) < time_diff:
@@ -8,14 +8,14 @@ def nearest_time(times):
             n_time = x
     return n_time
 
-def time_convert(x):
+def time_convert(x:str) -> int:
     hour = int(x[0:2])
     minute = int(x[3:5])
     second = int(x[6:8])
     time = (hour * 24 + minute) * 60 + second
     return time
 
-def c_time():
+def c_time() -> int:
     time = datetime.now()
     hour = int(time.strftime("%H"))
     minute = int(time.strftime("%M"))
@@ -23,6 +23,9 @@ def c_time():
     time2 = (hour * 24 + minute) * 60 + second
     return time2
 
-n = input().split(" ")
-print(n)
-print(nearest_time(n))
+def main():
+    n = input().split(" ")
+    print(nearest_time(n))
+
+if __name__ == "__main__":
+    main()
